22ea18cf24757def2216740e473a34b36cf164a5
lan <mxsudbek0808@gmail.com>
2026-07-02 14:45:56 +0300

    CORS tuzatildi

--- config/settings.py
+++ config/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'subscriptions',
     'drf_yasg',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,11 +122,16 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT=BASE_DIR/'media'
 
-STATIC_URL = 'static/'
+
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",                    # Frontend (Vite)
+    "https://gym1-m0wx.onrender.com",    # Netlify saytingiz
+]
 
+CORS_ALLOW_CREDENTIALS = True
